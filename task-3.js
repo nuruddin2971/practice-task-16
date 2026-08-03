e553aa9f1990e20670a47f1c31c3cf4346ba0eab
nuruddin2971 <nuruddin2971@gmail.com>
2026-08-03 18:29:22 +0600

    solved task-3.js

--- task-3.js
+++ task-3.js
@@ -1,3 +1,12 @@
-function generateReceipt(){
-    
+function generateReceipt(customarName, items, total) {
+  if (!Array.isArray(items) || items.length === 0) {
+    return "Invalid";
+  }
+
+  let receipt = `Receipt for ${customarName}\n${items}\nTotal: ${total}`;
+  return receipt;
 }
+
+console.log(generateReceipt("Rakib", ["Pen", "Book"], 150));
+console.log(generateReceipt("Sadia", ["Milk"], 60));
+console.log(generateReceipt("Tanvir", [], 0));
