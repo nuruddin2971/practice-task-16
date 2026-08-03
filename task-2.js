59fc380a1cdbb56cc0494171f92301069e5e8e1b
nuruddin2971 <nuruddin2971@gmail.com>
2026-08-03 17:57:39 +0600

    final commit for task2

--- task-2.js
+++ task-2.js
@@ -7,7 +7,12 @@ function bookTicket(movie, seats = 1, pricePerSeat = 300) {
   return total;
 }
 
-console.log(bookTicket("Dune"));
-console.log(bookTicket("Dune", 3));
-console.log(bookTicket("Dune", 2, 450));
-console.log(bookTicket(123, 2));
+const resutl1 = bookTicket("Dune");
+const resutl2 = bookTicket("Dune", 3);
+const resutl3 = bookTicket("Dune", 2, 450);
+const resutl4 = bookTicket(123, 2);
+
+console.log("Dune: 1 seat(s), Total Tk:", resutl1);
+console.log("Dune: 3 seat(s), Total Tk:", resutl2);
+console.log("Dune: 2 seat(s), Total Tk:", resutl3);
+console.log(resutl4);
