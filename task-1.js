08487c58c00b8607eba23df879d1f01cc5b03e9d
nuruddin2971 <nuruddin2971@gmail.com>
2026-08-03 16:41:44 +0600

    complete task1

---
+++ task-1.js
@@ -0,0 +1,18 @@
+// function describeDeclaration(string) {
+//   if (string !== "var" && string !== "let" && string !== "const") {
+//     return "Invalid";
+//   }
+//   if (string === "var") {
+//     return "Can redeclare, can reassign";
+//   }
+//   if (string === "let") {
+//     return "Cannot redeclare, can reassign";
+//   }
+//   if (string === "const") {
+//     return "Cannot redeclare, cannot reassign";
+//   }
+// }
+// console.log(describeDeclaration("let"));
+// console.log(describeDeclaration("const"));
+// console.log(describeDeclaration("var"));
+// console.log(describeDeclaration("int"));
